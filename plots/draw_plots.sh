ad5748e78cb65112e261ef4049c8a24bd3b87759
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-03-29 22:36:48 -0400

    changed to 200 Hz sampling rate

--- plots/draw_plots.sh
+++ plots/draw_plots.sh
@@ -2,5 +2,5 @@
 
 python3 fenton_plots.py
 python3 wave_sim_plots.py
-python3 wave_theories_plots.py
+python3 wave_theories_plots.py --formats png svg pgf --output-dir . --basename wave_sim-theories
 python3 wave_spectrum_plots.py
